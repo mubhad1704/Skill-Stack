830688fbc5d7feae58f85a3d68b8459b4f480213
mubhad0 <mubsdrive4@gmail.com>
2025-12-13 22:55:26 +0530

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer className="bg-linear-to-b from-[#393E46] via-[#2b2d31] to-[#1d2024]  py-4 mt-10 ">
+      <div className="max-w-7xl mx-auto text-center text-[#EEEEEE] text-sm">
+        &copy; {new Date().getFullYear()} SkillStack. All rights reserved.
+      </div>
+    </footer>
   )
 }
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 function LandingPage() {
   return (
     <div>
       <Navbar/>
-      
+
+      <Footer/>
     </div>
   )
 }
