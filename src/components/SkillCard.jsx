3e0fe9145f2175a5a278a3d1aa6ae5983ce5305f
mubhad0 <mubsdrive4@gmail.com>
2025-12-13 21:46:05 +0530

    Setup frontend folder structure

---
+++ src/components/SkillCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SkillCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SkillCard
---
+++ src/pages/SkillList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SkillList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SkillList
