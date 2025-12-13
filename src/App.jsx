1056a139fec28e47f800e5c49b5f3b1146f3c8f5
mubhad0 <mubsdrive4@gmail.com>
2025-12-13 22:01:46 +0530

    react-router-dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Dashboard from './pages/Dashboard'
+import LandingPage from './pages/LandingPage'
+import AddSkill from './pages/AddSkill'
+import SkillList from './pages/SkillList'
 
 function App() {
   
 
   return (
     <>
-      
+      <Routes>
+        <Route path='/'  element={<LandingPage/>}/>
+        <Route path='/dashboard'  element={<Dashboard/>}/>
+        <Route path='/add'  element={<AddSkill/>}/>
+        <Route path='/skill'  element={<SkillList/>}/>
+      </Routes>
     </>
   )
 }
