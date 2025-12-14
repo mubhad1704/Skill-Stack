b2eefcc5d1a94032c80649951a79dd7104339dab
mubhad0 <mubsdrive4@gmail.com>
2025-12-14 23:08:45 +0530

    Mobile responsive hamburger icon to navigate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tooltip from '@mui/material/Tooltip';
+import { FiMenu, FiX } from 'react-icons/fi';
 
 function Navbar() {
 
   const title = "SkillStack is a personal skill-building tracker for courses, tutorials, and certifications."
+  const [open, setOpen] = useState(false);
 
   return (
     <nav className=' bg-linear-to-b from-[#42454b] via-[#282a2e] to-[#17191d] shadow-md p-4'>
@@ -28,7 +30,33 @@ function Navbar() {
           </Tooltip>
           
         </div>
+
+        <div className="sm:hidden text-[#EEEEEE] text-2xl cursor-pointer">
+          {open ? (
+            <FiX onClick={() => setOpen(false)} />
+          ) : (
+            <FiMenu onClick={() => setOpen(true)} />
+          )}
+        </div>
       </div>
+
+       {open && (
+        <div className="sm:hidden mt-4 flex flex-col gap-4 text-[#EEEEEE] p-4 rounded-lg shadow-md">
+          <a href="/" onClick={() => setOpen(false)} className="hover:text-[#00ADB5]">
+            Home
+          </a>
+          <a href="/dashboard" onClick={() => setOpen(false)} className="hover:text-[#00ADB5]">
+            Dashboard
+          </a>
+          <a href="/add" onClick={() => setOpen(false)} className="hover:text-[#00ADB5]">
+            Add Skills
+          </a>
+          <a href="/skills" onClick={() => setOpen(false)} className="hover:text-[#00ADB5]">
+            View Skills
+          </a>
+
+        </div>
+      )}
     </nav>
   )
 }
